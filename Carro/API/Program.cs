8aaf852e3c0bbdca56eaaa2d5a6a9cce5fb8921d
ojeanfeli_pe <jean.felipe.moreira12@gmail.com>
2025-04-03 09:04:46 -0300

    Arquivos aula

--- Carro/API/Program.cs
+++ Carro/API/Program.cs
@@ -2,19 +2,63 @@ using API.Models;
 using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<AppDataContext>();
+
 var app = builder.Build();
 
-List<Carro> carros = new List<Carro>(){
-    new Carro {Id = 1, Name = "Fusca"},
-    new Carro {Id = 2, Name = "Ferrari"}
-};
-
-//Exemplo de requisição
-app.MapGet("/", () => "Hello World!");
-
-//endpoint listar carros
-app.MapGet("/api/carros", () => {
-    return Results.Ok(carros);
+//  Endpoints relacionados ao recurso de Carros
+//GET: Lista todos os carros cadastrados
+app.MapGet("/api/carros", ([FromServices] AppDataContext ctx) =>{
+    if(ctx.Carros.Any()){
+        return Results.Ok(ctx.Carros.ToList());
+    }
+    return Results.NotFound();
 });
+
+app.MapPost("/api/carros", ([FromBody]Carro carro,[FromServices] AppDataContext ctx) =>{
+
+        ctx.Carros.Add(carro);
+        ctx.SaveChanges();
+
+        return Results.Created("",carro);
+
+});
+
+app.MapGet("api/carros/{id}", ([FromRoute] int id, [FromServices] AppDataContext ctx) =>{
+
+    Carro? carro = ctx.Carros.Find(id);
+    if(carro != null){
+        return Results.Ok(carro);
+    }
+    return Results.NotFound();
+    
+});
+
+app.MapPut("api/carros/{id}", ([FromRoute] int id, [FromBody] Carro carro, [FromServices] AppDataContext ctx) =>{
+
+    Carro? carroUpdate = ctx.Carros.Find(id);
+
+   if   (carroUpdate != null){
+        carroUpdate.Name = carro.Name;
+        ctx.Carros.Update(carroUpdate);
+        ctx.SaveChanges();
+        return Results.Ok(carroUpdate);
+   }
+   return Results.NotFound();
+
+});
+
+app.MapDelete("api/carros/{id}", ([FromRoute] int id, [FromServices] AppDataContext ctx) =>{
+    
+    Carro? carro = ctx.Carros.Find(id);
+    
+    if(carro == null){
+        return Results.NotFound();
+    }
+    ctx.Carros.Remove(carro);
+    ctx.SaveChanges();
+    return Results.NoContent();
+});
+
 
 app.Run();
